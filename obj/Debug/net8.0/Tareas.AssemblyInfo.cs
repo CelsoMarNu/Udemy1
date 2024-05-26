ab49dff545235b79aa978f6c2ccaddd5d8d0e412
Celso <Celso_marnu@hotmail.com>
2024-05-26 16:36:19 -0400

    se agregan archivos compilados

--- obj/Debug/net8.0/Tareas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tareas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tareas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f846bce620f9cce8f7d55b628c10d1ca3b8bb1c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72ce02092018b662fddaaaaabd60cfa127c30f3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tareas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tareas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
